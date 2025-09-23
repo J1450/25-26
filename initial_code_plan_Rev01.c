19a0173eeb7a10a43d8abae5fdc652a4cbcce4e5
Jai Sen <136083540+J1450@users.noreply.github.com>
2025-09-23 16:42:53 -0400

    First revision of required modules for backend
    Just module definitions and surface level descriptions

---
+++ initial_code_plan_Rev01.c
@@ -0,0 +1,46 @@
+/* Code Cart Plan - Jai Sengupta / jaisengu
+ *
+ * Desc: Initial code plan for the code cart, specifically backend (C/py).
+ *
+ */
+
+/* Insert Lib here */
+
+/* Data structures here */
+
+/* Function prototypes here */
+
+/* Functions */.
+
+
+/* checkStock - Checks the stock of all contents being present and not
+ *              expired
+ */
+
+/* UpdateSupplies - Allows for the entering of new supplies, with NEEDED
+ *                  expiration date
+ */
+/* UpdateRequests - (a) Updates external hospital requests for restocks
+ *                  (b) Updates records of restock Requests
+ */
+
+/* chooseCodeType - Allows providers to choose the code type, saves information
+ *                  about the code type.
+ */
+
+/* init_codetype - Saves the code type requested, and time the code started
+ */
+
+/* -- Insert code types, and functions for specific codes -- */
+/* REQUIREMENTS REGARDLESS OF CODE: (a) Drawer LEDs for supplies needed
+ *                                  (b) LED off when supply taken
+ *                                  (c) If compressions, pole LED on
+ */
+
+/* overrideCode - Overrides the current code type */
+
+
+/* endCode - Saves information regarding the steps taken during the code,
+ *           which providers were there, and outcome of code, also
+ *           initiates checkStock */
+
