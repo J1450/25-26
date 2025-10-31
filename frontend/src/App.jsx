6d374d7945be09c32bd7cd286229d1b06600f4bb
nia <niabowrin@gmail.com>
2025-10-30 22:44:45 -0400

    Frontend file structure and pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<CodeSelectionPage />} />
+                <Route path="/code/asystole" element={<AsystolePage />} />
+                <Route path="/code/ventfib" element={<VentFibPage />} />
+                <Route path="/code/normalsinus" element={<NormalSinusPage />} />
+                <Route path="/code/summary" element={<SummaryPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
